6d0e390ff7e306f05190c329a89f2c638d709f0f
D-Pats <devinpatel97@gmail.com>
2022-05-26 16:28:08 +0100

    End of day 26.05.22

---
+++ Training Notes/Java Beginner/26.05.22/QAJavaWorkspace/AbstractionAndInterfaces/src/com/abstractionandinterfaces/Runner.java
@@ -0,0 +1,11 @@
+package com.abstractionandinterfaces;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Child test1 = new Child("Devin", 24);
+		System.out.println(test1);
+
+	}
+
+}
